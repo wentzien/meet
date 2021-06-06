c1e97e9043a04a186b00a020e692222377c75752
Dennis Wentzien <49585347+wentzien@users.noreply.github.com>
2021-06-06 21:42:51 +0200

    implementation of the meet process

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -2,12 +2,14 @@ import {Route, Switch, Redirect} from 'react-router-dom';
 
 import NotFound from "./pages/NotFound";
 import Home from "./pages/Home";
+import Meet from "./pages/Meet";
 
 function App() {
   return (
           <Switch>
               <Route path={"/404"} component={NotFound}/>
               <Route exact path={"/"} component={Home}/>
+              <Route exact path={"/meet/*"} component={Meet}/>
               <Route exact path={"/*"}>
                   <Redirect to={"/404"}/>
               </Route>
---
+++ react-app/src/pages/Meet.jsx
@@ -0,0 +1,88 @@
+import React, {useEffect, useRef} from "react";
+
+import io from "socket.io-client";
+import Peer from "peerjs";
+
+import "../App.css";
+
+const roomId = window.location.pathname.split("/")[1];
+const peer = new Peer();
+const socket = io("http://localhost:3000");
+const peers = {};
+
+const Meet = () => {
+    const refVideoGrid = useRef(null);
+    let videoGrid = null;
+
+    useEffect(() => {
+        videoGrid = refVideoGrid.current;
+
+        peer.on("open", id => {
+            navigator.mediaDevices.getUserMedia({
+                video: true,
+                audio: true
+            }).then(stream => {
+                const video = document.createElement("video");
+                video.muted = true;
+
+                addVideoStream(video, stream);
+
+                socket.emit("join-room", roomId, id);
+
+                peer.on("call", call => {
+                    call.answer(stream);
+
+                    const video = document.createElement("video");
+                    call.on("stream", userVideoStream => {
+                        addVideoStream(video, userVideoStream);
+                    });
+
+                    call.on("close", () => {
+                        video.remove();
+                    });
+
+                    peers[call.peer] = call;
+                });
+
+                socket.on("user-joined", userId => {
+                    connectToNewUser(userId, stream);
+                });
+
+                socket.on("user-disconnected", userId => {
+                    console.log("user disconnected", userId);
+                    console.log(peers);
+                    if (peers[userId]) peers[userId].close();
+                });
+            });
+        });
+    }, []);
+
+    const connectToNewUser = (userId, stream) => {
+        const call = peer.call(userId, stream);
+        const video = document.createElement("video");
+        call.on("stream", userVideoStream => {
+            addVideoStream(video, userVideoStream);
+        });
+        call.on("close", () => {
+            video.remove();
+        });
+
+        peers[userId] = call;
+    }
+
+    const addVideoStream = (video, stream) => {
+        video.srcObject = stream;
+        video.addEventListener("loadedmetadata", () => {
+            video.play();
+        });
+        videoGrid.append(video);
+    }
+
+    return (
+        <div id="video-grid" ref={refVideoGrid}>
+
+        </div>
+    );
+};
+
+export default Meet;
